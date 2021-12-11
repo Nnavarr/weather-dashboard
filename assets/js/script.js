c753464789782eb6748e136eb7bf62916a04cbd2
eon <noe.navarro1019@gmail.com>
2021-12-11 15:09:32 -0700

    the api pull for current day weather is now populating the following html elements: 1) temp, 2) speed, 3) humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
+// establish global variables
 var searchButtonEl = document.getElementById('search-btn');
+var tempEl = document.getElementById('temp');
+var windEl = document.getElementById('wind');
+var humidityEl = document.getElementById('humidity');
+var uvEl = document.getElementById('uv');
 
 var apiKey = '504f46843e11dd1aa60312828047eb5b';
 var cityObj = {
@@ -9,6 +14,9 @@ var cityObj = {
     'houston': 'texas'
 }
 
+var apiObj = {};
+
+// api call function
 var getCurrentWeather = function(){
 
     // api call based on user paramerter
@@ -16,17 +24,26 @@ var getCurrentWeather = function(){
     cityName = cityName.trim().toLowerCase();
 
     var state = cityObj[cityName];
-    var baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${state}&appid=${apiKey}`
+    var baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${state}&appid=${apiKey}&units=imperial`
 
-    var dataResponse =
-        fetch(baseUrl).then(function(response){
-            response.json().then(function(data) {
-                console.log(data);
-            });
+    // current day weather
+    fetch(baseUrl).then(function(response){
+        response.json().then(function(data) {
+            // update html containers to show data
+            console.log(data);
+            tempEl.innerHTML = tempEl.innerHTML + data.main.temp;
+            windEl.innerHTML = windEl.innerHTML + data.wind.speed + ' MPH';
+            humidityEl.innerHTML = humidityEl.innerHTML + data.main.humidity + ' %';
         });
-
-    return dataResponse;
+    });
 }
+
+// insert response values into appropriate html containers
+var populateResults = function(apiResponse){
+    humidityEl = apiResponse.main
+    console.log(humidityEl);
+}
+
 
 // event listeners
 searchButtonEl.addEventListener('click', getCurrentWeather);
